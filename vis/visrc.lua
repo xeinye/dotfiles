465c8efaeab45c54efd5418263af11c5f92d804a
xeinye <82726847+xeinye@users.noreply.github.com>
2024-07-06 00:32:30 -0300

    update

--- vis/visrc.lua
+++ vis/visrc.lua
@@ -1,12 +1,19 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
-
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
 	vis:command('set theme custom')
-end)
+	vis:command('set autoindent on')
+	vis:command('set ic on')
+	vis:command('set change-256colors off')
+	end)
+
+vis:map(vis.modes.NORMAL, '<C-p>', '"+p')
+vis:map(vis.modes.INSERT, '<C-p>', '<Escape>"+pa')
+vis:map(vis.modes.VISUAL_LINE, '<C-y>', function() vis:feedkeys(':>vis-clipboard --copy<Enter>') end)
+vis:map(vis.modes.VISUAL, '<C-y>', function() vis:feedkeys(':>vis-clipboard --copy<Enter>') end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- Your per window configuration options e.g.
-	 vis:command('set number')
+	vis:command('set show-eof off')
 end)
